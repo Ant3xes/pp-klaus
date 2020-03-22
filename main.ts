833f0daf8ea54975cdad5e3979e2a6ad7482007f
Ant3xes <pereira.romain01@gmail.com>
2020-03-22 16:41:34 +0100

    b

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-import Pony from './class/PonyClass'
-import DragonBall, { DBHeroe } from './class/DragonBallClass'
+import Pony from './class/Pony'
+import DragonBall, { DragonBHeroe } from './class/DragonB'
 
 console.log("<-- STEP 1 -->");
 
 const fisrtPony: Pony = new Pony();
 const secondPony: Pony = new Pony();
 
-const sangoku: DragonBall = new DragonBall(DBHeroe.SANGOKU);
+const sangoku: DragonBall = new DragonBall(DragonBHeroe.SANGOKU);
 
 fisrtPony.isMoved()
 sangoku.isMoved()
